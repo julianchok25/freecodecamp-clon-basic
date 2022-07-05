a4ccc035a731354a19cd050b8d108bbbaae9e643
Julian Villegas <julian.villegasplus@gmail.com>
2022-07-04 23:09:39 -0500

    [UPD]App: Changing functional components by class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
 import Testimony from './components/Testimony';
 import { TESTIMONIES } from './static/json/testimonies';
 
-function App() {
-	const testimoniesData = [...TESTIMONIES];
+class App extends React.Component {
+	testimoniesData = [...TESTIMONIES];
 
-	const testimony = () => {
+	testimony = () => {
 		return (
 			<>
 				Siempre he tenido problemas para aprender JavaScript.
@@ -19,25 +20,27 @@ function App() {
 		);
 	};
 
-	return (
-		<div className='App'>
-			<div className='main-container'>
-				<h1>Esto es lo que dicen nuestros alumnos:</h1>
-				{testimoniesData.map((item) => {
-					return <Testimony {...item} />;
-				})}
+	render() {
+		return (
+			<div className='App'>
+				<div className='main-container'>
+					<h1>Esto es lo que dicen nuestros alumnos:</h1>
+					{this.testimoniesData.map((item) => {
+						return <Testimony {...item} />;
+					})}
 
-				<Testimony
-					name='Emma Bostian'
-					country='Suecia'
-					imageName='emma'
-					job='Software Engineer'
-					company='Spotify'
-					testimony={testimony()}
-				/>
+					<Testimony
+						name='Emma Bostian'
+						country='Suecia'
+						imageName='emma'
+						job='Software Engineer'
+						company='Spotify'
+						testimony={this.testimony()}
+					/>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import '../css/Testimony.css';
 
-function Testimony(props) {
-	console.log(props);
-
-	return (
-		<div className='container'>
-			<img
-				className='image'
-				src={require(`../static/images/testimony-${props.imageName}.png`)}
-				alt={`${props.imageName} testimony`}
-			/>
-			<div className='text-content'>
-				<p className='country-name'>
-					<strong>{props.name}</strong> en {props.country}
-				</p>
-				<p className='job'>
-					{props.job} en <strong>{props.company}</strong>
-				</p>
-				<p className='description'>"{props.testimony}"</p>
+class Testimony extends React.Component {
+	render() {
+		return (
+			<div className='container'>
+				<img
+					className='image'
+					src={require(`../static/images/testimony-${this.props.imageName}.png`)}
+					alt={`${this.props.imageName} testimony`}
+				/>
+				<div className='text-content'>
+					<p className='country-name'>
+						<strong>{this.props.name}</strong> en{' '}
+						{this.props.country}
+					</p>
+					<p className='job'>
+						{this.props.job} en{' '}
+						<strong>{this.props.company}</strong>
+					</p>
+					<p className='description'>
+						"{this.props.testimony}"
+					</p>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default Testimony;
